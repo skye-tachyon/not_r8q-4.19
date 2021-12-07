0d43377ec65551b79d87acc4b45c8a3d048cc5ce
Divyanshu-Modi <divyan.m05@gmail.com>
2021-12-07 11:35:14 +0530

    Usb: gadget: f_gsi: compile out ipc logging conditionally
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/usb/gadget/function/f_gsi.c
+++ drivers/usb/gadget/function/f_gsi.c
@@ -3578,8 +3578,10 @@ static int gsi_set_inst_name(struct usb_function_instance *fi,
 {
 	int prot_id, name_len;
 	struct f_gsi *gsi;
+#ifdef CONFIG_IPC_LOGGING
 	char gsi_inst_name[MAX_INST_NAME_LEN + sizeof("gsi.") + 1];
 	void *ipc_log_ctxt;
+#endif
 	struct gsi_opts *opts, *opts_prev;
 
 	opts = container_of(fi, struct gsi_opts, func_inst);
@@ -3614,6 +3616,7 @@ static int gsi_set_inst_name(struct usb_function_instance *fi,
 	if (IS_ERR(gsi))
 		return PTR_ERR(gsi);
 
+#ifdef CONFIG_IPC_LOGGING
 	opts->gsi = gsi;
 	/*
 	 * create instance name with prefixing "gsi." to differentiate
@@ -3625,6 +3628,7 @@ static int gsi_set_inst_name(struct usb_function_instance *fi,
 		pr_err("%s: Err allocating ipc_log_ctxt for prot:%s\n",
 						__func__, gsi_inst_name);
 	opts->gsi->ipc_log_ctxt = ipc_log_ctxt;
+#endif
 
 #ifdef CONFIG_USB_ANDROID_SAMSUNG_COMPOSITE
 	if (prot_id != IPA_USB_RNDIS)
