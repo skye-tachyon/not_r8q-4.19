262490f4d46543f341f51f36fc7f7366319113d0
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2023-09-20 15:00:24 +0200

    sched/debug: Remove the /proc/sys/kernel/sched_child_runs_first sysctl
    The /proc/sys/kernel/sched_child_runs_first knob is no longer connected since:
    
       5e963f2bd4654 ("sched/fair: Commit to EEVDF")
    
    Remove it.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lore.kernel.org/r/20230920130025.412071-2-bigeasy@linutronix.de

--- include/linux/sched/sysctl.h
+++ include/linux/sched/sysctl.h
@@ -30,7 +30,11 @@ extern unsigned int sysctl_sched_min_granularity;
 extern unsigned int sysctl_sched_sync_hint_enable;
 extern unsigned int sysctl_sched_cstate_aware;
 extern unsigned int sysctl_sched_wakeup_granularity;
+<<<<<<< HEAD
 extern unsigned int sysctl_sched_child_runs_first;
+=======
+#ifdef CONFIG_SCHED_WALT
+>>>>>>> bd43c21df9b41 (sched/debug: Remove the /proc/sys/kernel/sched_child_runs_first sysctl)
 extern unsigned int sysctl_sched_force_lb_enable;
 #ifdef CONFIG_SCHED_WALT
 extern unsigned int sysctl_sched_capacity_margin_up[MAX_MARGIN_LEVELS];
