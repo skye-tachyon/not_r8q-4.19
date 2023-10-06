0aabb5dc27e9ee0b782542b1f8fb3e5126414a06
Tim Zimmermann <tim@linux4.de>
2023-10-06 05:32:56 +0200

    [SQUASH] Fake uname to 6.6.40
    Commits:
    
    syscall: Fake uname to 4.19 for bpfloader/netd
    da01aa1561489cd258dd456c14dd300e511ff34b
    
    syscall: Fake uname to 4.19 also for netbpfload
    9df76d483462e0c5972fb1a7cb3efd8b662f0d3a
    
    syscall: Increase bpf fake uname to 5.4
    a4e31cb4fe1a6a11a5b4c47eea43626d0640c072
    
    syscall: Increase bpf fake uname to 5.4.186
    04ab1a48dd1ec2b3c901b98675be3056ba7fae4b
    
    syscall: Increase fake uname to 6.6.40
    96eea0e08f1df985c6944d2189d582522bf8ce9d

--- kernel/sys.c
+++ kernel/sys.c
@@ -1243,12 +1243,20 @@ static int override_release(char __user *release, size_t len)
 #ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
 extern void susfs_spoof_uname(struct new_utsname* tmp);
 #endif
+
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
+	if (!strncmp(current->comm, "bpfloader", 9) ||
+	    !strncmp(current->comm, "netbpfload", 10) ||
+	    !strncmp(current->comm, "netd", 4)) {
+		strcpy(tmp.release, "6.6.40");
+		pr_debug("fake uname: %s/%d release=%s\n",
+			 current->comm, current->pid, tmp.release);
+	}
 #ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
 	susfs_spoof_uname(&tmp);
 #endif
