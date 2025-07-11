c7cbb71d9c19e48072504a6b546b74ece991b77c
skye-pa1n <sonicgames772@gmail.com>
2025-07-11 15:23:28 -0300

    include: linux: Update

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -375,7 +375,6 @@ struct cpufreq_driver {
 						   transitions */
 #define CPUFREQ_PM_NO_WARN	(1 << 2)	/* don't warn on suspend/resume
 						   speed mismatches */
-
 /*
  * This should be set by platforms having multiple clock-domains, i.e.
  * supporting multiple policies. With this sysfs directories of governor would
