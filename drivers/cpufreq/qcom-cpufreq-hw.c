d20ed40dd935312b6ffd86fce55c8207c2be470c
skye-pa1n <sonicgames772@gmail.com>
2025-12-29 09:11:02 -0300

    cpufreq: qcom-hw: Remove samsung garbage

--- drivers/cpufreq/qcom-cpufreq-hw.c
+++ drivers/cpufreq/qcom-cpufreq-hw.c
@@ -18,7 +18,6 @@
 #include <linux/sched.h>
 #include <linux/cpu_cooling.h>
 
-#include <linux/sec_debug.h>
 #include <trace/events/power.h>
 
 #define CREATE_TRACE_POINTS
@@ -35,11 +34,6 @@
 
 #define CYCLE_CNTR_OFFSET(core_id, m, acc_count)		\
 			(acc_count ? ((core_id + 1) * 4) : 0)
-
-
-#ifdef CONFIG_SEC_PM
-extern void *thermal_ipc_log;
-#endif
 
 enum {
 	CPUFREQ_HW_LOW_TEMP_LEVEL,
@@ -94,10 +88,6 @@ struct cpufreq_qcom {
 	char dcvsh_irq_name[MAX_FN_SIZE];
 	bool is_irq_enabled;
 	bool is_irq_requested;
-#ifdef CONFIG_SEC_PM
-	unsigned long lowest_freq;
-	bool limiting;
-#endif
 };
 
 struct cpufreq_counter {
@@ -185,15 +175,6 @@ static unsigned long limits_mitigation_notify(struct cpufreq_qcom *c,
 	trace_dcvsh_freq(cpumask_first(&c->related_cpus), freq);
 	c->dcvsh_freq_limit = freq;
 
-#ifdef CONFIG_SEC_PM
-	if (c->limiting == false) {
-		THERMAL_IPC_LOG("Start lmh cpu%d @%lu\n",
-			cpumask_first(&c->related_cpus), freq);
-		c->lowest_freq = freq;
-		c->limiting = true;
-	}
-#endif
-
 	return freq;
 }
 
@@ -213,10 +194,6 @@ static void limits_dcvsh_poll(struct work_struct *work)
 	dcvsh_freq = qcom_cpufreq_hw_get(cpu);
 
 	if (freq_limit != dcvsh_freq) {
-#ifdef CONFIG_SEC_PM
-	if ((c->limiting == true) && (freq_limit < c->lowest_freq))
-			c->lowest_freq = freq_limit;
-#endif
 		mod_delayed_work(system_highpri_wq, &c->freq_poll_work,
 				msecs_to_jiffies(LIMITS_POLLING_DELAY_MS));
 	} else {
@@ -229,13 +206,6 @@ static void limits_dcvsh_poll(struct work_struct *work)
 
 		c->is_irq_enabled = true;
 		enable_irq(c->dcvsh_irq);
-#ifdef CONFIG_SEC_PM
-		THERMAL_IPC_LOG("Fin. lmh cpu%d, "
-			"lowest %lu, f_lim %lu, dcvsh %lu\n",
-			cpu, c->lowest_freq, freq_limit, dcvsh_freq);
-		c->limiting = false;
-		c->lowest_freq = UINT_MAX;
-#endif
 	}
 
 	mutex_unlock(&c->dcvsh_lock);
