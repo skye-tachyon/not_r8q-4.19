e10ea4d0301c9447840e91e84ce45b1166c105f0
@skye <sonicgames772@gmail.com>
2026-01-01 23:32:10 -0300

    build: build without LTO

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ NC='\033[0m'
 ZIPNAME="not-$(date '+%Y%m%d-%H%M')-r8q.zip"
 TC_DIR="$(pwd)/tc/clang-r522817"
 AK3_DIR="$(pwd)/AnyKernel3"
-DEFCONFIG="vendor/kona-not_defconfig vendor/samsung/kona-sec-not.config vendor/samsung/r8q.config vendor/not/no_werror.config"
+DEFCONFIG="vendor/kona-not_defconfig vendor/samsung/kona-sec-not.config vendor/samsung/r8q.config vendor/not/no_werror.config vendor/not/no_lto.config"
 
 OUT_DIR="$(pwd)/out"
 BOOT_DIR="$OUT_DIR/arch/arm64/boot"
